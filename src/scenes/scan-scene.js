1084fdfe9d83cdd672f355cee9d2815f931adb70
KPlusa <kacper.plusaa@gmail.com>
2021-11-05 18:08:01 +0100

    fixed scan scene

--- src/scenes/scan-scene.js
+++ src/scenes/scan-scene.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
-import {Text, View, StyleSheet, StatusBar, Image} from 'react-native';
+import {View, StyleSheet, StatusBar, Image} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import {Overlay, Divider} from 'react-native-elements';
+import {Divider, Text} from 'react-native-elements';
 import Background from '../components/background';
 import Button from '../components/button';
 import {theme} from '../themes/theme';
@@ -9,93 +9,140 @@ import ImagePicker from 'react-native-image-crop-picker';
 
 export default function Scan({navigation}) {
   const [pickedImage, setPickedImage] = useState(null);
+  const [mimeImage, setMimeImage] = useState(null);
   const [showResetButton, setShowResetButton] = useState(false);
-
-const [image, setImage] = useState('https://api.adorable.io/avatars/80/abott@adorable.png');
-
-
-  const takePhotoFromCamera = () => {
-    ImagePicker.openCamera({
-      compressImageMaxWidth: 300,
-      compressImageMaxHeight: 300,
-      cropping: false,
-      compressImageQuality: 0.7,
-      rotation: 360
-    }).then(image => {
-      console.log(image);
-      setImage(image.path);
-      this.bs.current.snapTo(1);
-    });
-  };
 
   reset = () => {
     setPickedImage(null);
+    setMimeImage(null);
+  };
+  const takePhotoFromCamera = () => {
+    reset();
+    ImagePicker.openCamera({
+      compressImageMaxWidth: 960,
+      compressImageMaxHeight: 1280,
+      cropping: false,
+      includeBase64: true,
+    }).then(image => {
+      setPickedImage(image.data);
+      setMimeImage(image.mime);
+    });
   };
 
-const takePhotoFromGalery = () => {
-  ImagePicker.openPicker({
-  width: 300,
-  height: 400,
-  cropping: false,
-  rotation: nonZeroInteger
-}).then(image => {
-  console.log(image);
-});
-};
-  resetHandler = () => {
+  const takePhotoFromGalery = () => {
     reset();
+    ImagePicker.openPicker({
+      width: 1280,
+      height: 960,
+      cropping: false,
+      includeBase64: true,
+    }).then(image => {
+      setPickedImage(image.data);
+      setMimeImage(image.mime);
+    });
   };
+
+
   return (
     <Background>
-      
       <View elevation={4} style={styles.rectangle}>
-      <View style={styles.placeholder}>
-        <Image source={pickedImage} style={styles.previewImage} />
-      </View>
-        <View>
-          <Button
-            icon={
-              <Icon
-                name="image"
-                size={20}
-                color="white"
-                style={{marginRight: 10}}
-              />
-            }
-            style={[
-              {height: 50},
-              {width: 300},
-              {backgroundColor: theme.colors.mainColor},
-              {alignSelf: 'flex-start'},
-            ]}
-            txtStyle={{justifyContent: 'center'}}
-            title="Pick Image"
-            onPress={takePhotoFromGalery}
-          />
-          <Divider orientation="horizontal" height={20} />
-          <Button
-            icon={
-              <Icon
-                name="camera"
-                size={20}
-                color="white"
-                style={{marginRight: 10}}
-              />
-            }
-            style={[
-              {height: 50},
-              {width: 300},
-              {backgroundColor: theme.colors.mainColor},
-              {alignSelf: 'flex-start'},
-            ]}
-            txtStyle={{justifyContent: 'center'}}
-            title="Take Photo"
-            onPress={takePhotoFromCamera}
-          />
-        </View>
-      </View>
-      <View style={styles.button}>
-        {pickedImage ? <Button title="Reset" onPress={resetHandler} /> : null}
+        {pickedImage ? (
+          <View style={styles.container}>
+            <Text style={styles.textStyle}>Preview:</Text>
+            <View style={styles.placeholder}>
+              <Image
+                source={{uri: `data:${mimeImage};base64,${pickedImage}`}}
+                style={styles.previewImage}></Image>
+            </View>
+          </View>
+        ) : null}
+
+        {pickedImage ? (
+          <View>
+            <Divider orientation="horizontal" height={50} />
+            <Button
+              icon={
+                <Icon
+                  name="file"
+                  size={20}
+                  color="white"
+                  style={{marginRight: 10}}
+                />
+              }
+              style={[
+                {height: 50},
+                {width: 300},
+                {backgroundColor: theme.colors.mainColor},
+                {alignSelf: 'flex-start'},
+              ]}
+              txtStyle={{justifyContent: 'center'}}
+              title="Extract Data"
+              onPress={() => console.log(pickedImage)}
+            />
+            <Divider orientation="horizontal" height={20} />
+            <Button
+              icon={
+                <Icon
+                  name="undo"
+                  size={20}
+                  color="white"
+                  style={{marginRight: 10}}
+                />
+              }
+              style={[
+                {height: 50},
+                {width: 300},
+                {backgroundColor: theme.colors.mainColor},
+                {alignSelf: 'flex-start'},
+              ]}
+              txtStyle={{justifyContent: 'center'}}
+              title="Reset Photo"
+              onPress={reset}
+            />
+          </View>
+        ) : (
+          <View>
+            <Button
+              icon={
+                <Icon
+                  name="image"
+                  size={20}
+                  color="white"
+                  style={{marginRight: 10}}
+                />
+              }
+              style={[
+                {height: 50},
+                {width: 300},
+                {backgroundColor: theme.colors.mainColor},
+                {alignSelf: 'flex-start'},
+              ]}
+              txtStyle={{justifyContent: 'center'}}
+              title="Pick Image"
+              onPress={takePhotoFromGalery}
+            />
+            <Divider orientation="horizontal" height={20} />
+            <Button
+              icon={
+                <Icon
+                  name="camera"
+                  size={20}
+                  color="white"
+                  style={{marginRight: 10}}
+                />
+              }
+              style={[
+                {height: 50},
+                {width: 300},
+                {backgroundColor: theme.colors.mainColor},
+                {alignSelf: 'flex-start'},
+              ]}
+              txtStyle={{justifyContent: 'center'}}
+              title="Take Photo"
+              onPress={takePhotoFromCamera}
+            />
+          </View>
+        )}
       </View>
     </Background>
   );
@@ -103,7 +150,8 @@ const takePhotoFromGalery = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: '70%',
+    height: '50%',
     alignItems: 'center',
     justifyContent: 'center',
   },
@@ -120,12 +168,10 @@ const styles = StyleSheet.create({
     marginTop: 10,
   },
   placeholder: {
-    borderWidth: 1,
-    borderColor: 'black',
-    backgroundColor: '#eee',
-    width: '70%',
-    height: 280,
-    marginTop: 50,
+    width: '100%',
+    height: '100%',
+    marginTop: 20,
+    marginBottom: 20,
   },
   button: {
     width: '80%',
@@ -135,6 +181,7 @@ const styles = StyleSheet.create({
   previewImage: {
     width: '100%',
     height: '100%',
+    borderRadius: 10,
   },
   rectangle: {
     width: '100%',
