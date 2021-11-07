dabf0ff410d428e28af501118b5d7700da6b708a
KPlusa <kacper.plusaa@gmail.com>
2021-11-07 17:05:55 +0100

    fixed behaviour of resizing logo

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import {Image, StyleSheet} from 'react-native';
-import { baseProps } from 'react-native-gesture-handler/lib/typescript/handlers/gestureHandlers';
 
-export default function Logo(stylee, ...props) {
+export default function Logo(style, ...props) {
   return (
-    <Image source={require('../assets/images/logo.png')} style={styles.image, stylee} {...props} />
+    <Image source={require('../assets/images/logo.png')} style={[styles.image,style]}{...props}  />
   );
 }
 
 const styles = StyleSheet.create({
   image: {
     width: 300,
-    height: 200,
-    
+    height: 200,   
   },
 });
